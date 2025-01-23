63de5f0c9a6767dd749b119702ae1e98e2bb732f
snil4 <snil4@walla.co.il>
2025-01-24 01:53:41 +0200

    added win signal and timer end signal

--- godot/global/gameplay_global.gd
+++ godot/global/gameplay_global.gd
@@ -8,6 +8,9 @@ signal goal(is_player_1: bool)
 signal goal_reset()
 signal timer()
 signal timer_updated()
+signal player_win(is_player_1: bool)
+signal game_tie()
+signal timer_end()
 
 
 func _ready():
@@ -25,7 +28,12 @@ func on_goal(is_player_1: bool):
 
 	await get_tree().create_timer(2.0).timeout
 
-	goal_reset.emit()
+	if score1 < 5 && score2 < 5:
+		goal_reset.emit()
+	elif score1 >= 5:
+		player_win.emit(true)
+	elif score2 >= 5:
+		player_win.emit(false)
 
 
 func on_goal_reset():
@@ -34,5 +42,14 @@ func on_goal_reset():
 
 func on_timer():
 	time += 1
-
 	timer_updated.emit()
+
+
+func on_timer_end():
+
+	if score1 > score2:
+		player_win.emit(true)
+	elif score2 > score1:
+		player_win.emit(false)
+	else:
+		game_tie.emit()
--- godot/ui/components/timer_label.gd
+++ godot/ui/components/timer_label.gd
@@ -1,10 +1,14 @@
 extends Label
 
+@export var countdown = true
+@export var countdown_seconds = 60
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# default text
-	text = "00:00"
+
+	# initialize timer
+	on_timer()
 
 	# connect timer update signal to update text
 	GameplayGlobal.timer_updated.connect(on_timer)
@@ -12,16 +16,28 @@ func _ready() -> void:
 
 # when timer updates - update text
 func on_timer():
+
 	# calculate seconds
-	var seconds_int = GameplayGlobal.time % 60
+	var seconds_int = 0
+	if countdown:
+		seconds_int = (countdown_seconds - GameplayGlobal.time) % 60
+	else:
+		seconds_int = GameplayGlobal.time % 60
 	var seconds_str = get_time_str(seconds_int)
 
 	# calculate minutes
-	var minutes_int = GameplayGlobal.time / 60
+	var minutes_int = 0
+	if countdown:
+		minutes_int = (countdown_seconds - GameplayGlobal.time) / 60
+	else:
+		minutes_int = GameplayGlobal.time / 60
 	var minutes_str = get_time_str(minutes_int)
 
 	# concat final text
 	text = minutes_str + ":" + seconds_str
+
+	if countdown && seconds_int == 0 && minutes_int == 0:
+		GameplayGlobal.timer_end.emit()
 
 
 # get time string
