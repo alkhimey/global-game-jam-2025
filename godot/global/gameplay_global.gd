ae55a915f974868ac3ed8706283d4144eb17a2f2
snil4 <snil4@walla.co.il>
2025-01-24 00:35:48 +0200

    added ui elements

--- godot/global/gameplay_global.gd
+++ godot/global/gameplay_global.gd
@@ -1,11 +1,29 @@
 extends Node
 
+var score1: int = 0
+var score2: int = 0
+var time: int = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+signal goal(is_player_1: bool)
+signal goal_reset()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _ready():
+	goal.connect(on_goal)
+	goal_reset.connect(on_goal_reset)
+
+
+func on_goal(is_player_1: bool):
+
+	if is_player_1:
+		score1 += 1
+	else:
+		score2 += 1
+
+	await get_tree().create_timer(2.0).timeout
+
+	goal_reset.emit()
+
+
+func on_goal_reset():
 	pass
