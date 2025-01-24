46eaf92013a7036267e93edef5a6b0c7fc43656b
lordlolipop <liron.kol.8@gmail.com>
2025-01-24 02:08:45 +0200

    resolve conflicts with speech bubble

--- godot/scenes/prototype/player_bubble.gd
+++ godot/scenes/prototype/player_bubble.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 @export var playerName: String
+@onready var label: Label = $Label
+
+
 
 @export var left_input_name: String = "player1_left"
 @export var right_input_name: String = "player1_right"
@@ -15,8 +18,7 @@ extends CharacterBody2D
 @export var gravity : float = 1000
 
 func _ready() -> void:
-	var topTextLabel = $MarginContainer/Label
-	topTextLabel.text = playerName
+	display_text() ##use when player lost a round, connet  pla <3
 
 func _physics_process(delta: float) -> void:
 	apply_gravity(delta)
@@ -46,6 +48,16 @@ func jump():
 	if is_on_floor():
 		if Input.is_action_just_pressed(up_input_name):
 			velocity.y = jump_velocity
+			label.show()
 	else: #חצי קפיצה בשיחרור המקש
 		if Input.is_action_just_released(up_input_name) and velocity.y < jump_velocity / 2:
 				velocity.y = jump_velocity / 2
+				
+
+
+func display_text():
+	var write_speed := 3
+	var tween := create_tween()
+	var duration := label.text.length() / write_speed
+	tween.tween_property(label, "visible_ratio", 1.0, duration)\
+		.from(0.0)
