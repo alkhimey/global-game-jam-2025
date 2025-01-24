d72a005aca40807905b93cfd5d8b6ea13303ae3f
snil4 <snil4@walla.co.il>
2025-01-24 16:36:08 +0200

    audience is working

--- godot/scenes/prototype/audience.gd
+++ godot/scenes/prototype/audience.gd
@@ -1,24 +1,27 @@
 extends Node2D
 
 @export var audience_scene: PackedScene
+@export var inbetween_seconds: float = 0.1
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	GameplayGlobal.goal.connect(on_goal)
+	GameplayGlobal.player_win.connect(on_win)
 
-	summon_audience(4, 0)
 	summon_audience(4, 1)
+	summon_audience(4, 2)
 
 
-func on_goal(playerId: int):
-	summon_audience(3, playerId - 1)
+func on_win(playerId: int):
+	if playerId > 0:
+		summon_audience(3, playerId - 1)
 
 
-func summon_audience(amount: int, color_num: int):
+func summon_audience(amount: int, player_num: int):
 	for i in amount:
 		var audience_node = audience_scene.instantiate()
-		audience_node.shirt_color = GameplayGlobal.player_colors[color_num]
-		audience_node.change_shirt()
 
-		add_child(audience_scene.instantiate())
+		add_child(audience_node)
+		audience_node.change_player(player_num)
+
+		await get_tree().create_timer(inbetween_seconds).timeout
--- godot/scenes/prototype/audience_human.gd
+++ godot/scenes/prototype/audience_human.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
 var shirt_color: Color = Color.WHITE
-var current_speed: float
-var initial_height: float
-var playerId
+var initial_width: float
+var playerId: int
+var sign_type: int
+var skin_color: int
 
-@export var skin_colors: Array = [Color.SALMON, Color.BROWN, Color.LIGHT_PINK, Color.SANDY_BROWN]
+@export var speed: float = 5
+@export var starting_side: Array = [-20, -10, 864]
+@export var starting_height: Array = [226, 186, 276]
+@export var body_sprites: Array = []
+@export var eyes_sprites: Array = []
+@export var head_sprites: Array = []
 @export var jump_offset: Vector2 = Vector2(0, 3)
 @export var normal_speed: float = 5
 @export var excite_speed: float = 0.5
@@ -13,17 +19,16 @@ var playerId
 
 @onready var body_node = $Body
 @onready var head_node = $Head
+@onready var hand_node = $Hands
+@onready var head_sign_node = $Hands/HeadSign
+@onready var body_sign_node = $Hands/BodySign
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GameplayGlobal.goal.connect(on_goal)
-	initial_height = position.y
-	change_speed(1)
-
-	change_shirt()
-	head_node.modulate = skin_colors.pick_random()
-	start_tween()
+	position.y = starting_height.pick_random()
+	# change_speed(1)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -32,27 +37,50 @@ func _process(_delta: float) -> void:
 
 
 func start_tween():
-	var tween = create_tween().set_loops()
+	var tween = create_tween()
 
-	tween.tween_property(self, "position:y", initial_height + jump_offset.y, current_speed).set_trans(Tween.TRANS_QUART)
-	tween.tween_property(self, "position:y", initial_height, current_speed).set_trans(Tween.TRANS_QUART)
-
-
-func change_player(playerId: int):
-	body_node.modulate = shirt_color
+	tween.tween_property(self, "position:x", initial_width, speed).set_trans(Tween.TRANS_QUART)
 
 
-func change_speed(speed_level: int):
-	match speed_level:
+func change_player(_playerId: int):
+
+	sign_type = randi_range(0,2)
+	match sign_type:
+		0:
+			pass
 		1:
-			current_speed = normal_speed
+			body_sign_node.visible = true
 		2:
-			current_speed = excite_speed
-		3:
-			current_speed = goal_speed
+			head_sign_node.visible = true
+
+	playerId = _playerId
+	position.x = starting_side[_playerId]
+
+	if playerId > 0:
+		shirt_color = GameplayGlobal.player_colors[playerId - 1]
+
+	body_node.modulate = shirt_color
+	head_sign_node.modulate = shirt_color
+	body_sign_node.modulate = shirt_color
+
+	match playerId:
+		1:
+			initial_width = randf_range(0., 854. / 2.)
+		2:
+			initial_width = randf_range(854. / 2., 854.)
+
+	if playerId > 0:
+		start_tween()
 
 
 func on_goal(_playerId: int):
 	if _playerId == playerId:
-		change_speed(3)
-		start_tween()
+		pass
+		# change_speed(3)
+		# start_tween()
+
+
+func almost_goal():
+	pass
+	# change_speed(2)
+	# start_tween()
