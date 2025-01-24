69143942e699406f14d92142a3348e53a00dcbb2
snil4 <snil4@walla.co.il>
2025-01-24 02:02:19 +0200

    added gameplay overlay scene

--- godot/ui/components/score_label.gd
+++ godot/ui/components/score_label.gd
@@ -3,10 +3,10 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	text = "0 : 0"
+	text = "0: 0"
 
 	GameplayGlobal.goal.connect(on_goal)
 
 
 func on_goal(_is_player_1: bool):
-	text = str(GameplayGlobal.score1) + " : " + str(GameplayGlobal.score2)
+	text = str(GameplayGlobal.score1) + ": " + str(GameplayGlobal.score2)
