e1df56220a4164c4c0d55dfeeb882a584448ccb5
Artium Nihamkin <artium@nihamkin.com>
2025-01-25 01:11:00 +0200

    arrow animation and rainbow shader from godot site

--- godot/scenes/prototype/goal.gd
+++ godot/scenes/prototype/goal.gd
@@ -3,9 +3,21 @@ extends Node2D
 @export var playerId: int
 @export var arrow: Sprite2D
 
+var arrowPresentationTimeSeconds: float = 5.0
+var arrowOrigPos: Vector2
 
 func _ready() -> void:
+	arrowPresentationTimeSeconds = Time.get_ticks_msec()
+	arrowOrigPos = arrow.position
 	pass
+	
+func _process(delta: float) -> void:
+	if Time.get_ticks_msec() - arrowPresentationTimeSeconds > 5 * 1000:
+		arrow.hide()
+		
+	
+	arrow.position.y = arrowOrigPos.y + 80.0 * cos(
+		(Time.get_ticks_msec() - arrowPresentationTimeSeconds) / 200 + PI)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:	
 	if not GameplayGlobal.can_goal:
