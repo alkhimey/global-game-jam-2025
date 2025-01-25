83b3132f85e45a659aecc60d645ae43404e90f29
Artium Nihamkin <artium@nihamkin.com>
2025-01-25 17:32:41 +0200

    in level ui - score and timer

--- godot/ui/overlays/score_label_normal.gd
+++ godot/ui/overlays/score_label_normal.gd
@@ -8,7 +8,14 @@ func _ready() -> void:
 	GameplayGlobal.goal.connect(on_goal)
 
 func on_goal(_playerId: int):
+	if _playerId == playerId:
+		var origScale = self.scale
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "scale", Vector2(1.5, 1.5), 0.3)
+		tween.tween_property(self, "scale", origScale, 0.3)
+
 	if playerId == 1:
 		self.text = str(GameplayGlobal.score1)
 	else:
 		self.text = str(GameplayGlobal.score2)
+		
