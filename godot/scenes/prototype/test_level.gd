fb826da78dff530a6700f4655ea2b85186b4d2eb
Artium Nihamkin <artium@nihamkin.com>
2025-01-25 16:00:25 +0200

    adding movement for goal

--- godot/scenes/prototype/test_level.gd
+++ godot/scenes/prototype/test_level.gd
@@ -4,12 +4,39 @@ extends Node2D
 @onready var gameover_overlay = $CanvasLayer/GameoverOverlay
 @onready var music_player: AudioStreamPlayer = $MusicPlayer
 
+var levelTimer: Timer
+@export var goal: Node2D
+
+var currentGoalPositionPreset = 0
+var goalPositionPresets: Array
+var nextGoalPositionWhenTimeLeft: Array 
 
 func _ready():
 	GameplayGlobal.player_win.connect(on_game_over)
 
 	GameplayGlobal.game_reset.emit()
 
+	levelTimer = $CanvasLayer/GameplayOverlay/LevelTimer
+
+	goalPositionPresets = [
+		goal.position,
+		Vector2(210,228),
+		Vector2(678, 261)
+	]
+	
+	nextGoalPositionWhenTimeLeft = [
+		(GameplayGlobal.countdown_time * 2.0) / 3.0,
+		GameplayGlobal.countdown_time / 3.0
+	]
+
+func _process(delta: float) -> void:
+	if not levelTimer.is_stopped():
+		if currentGoalPositionPreset < goalPositionPresets.size() - 1:
+			if levelTimer.time_left < nextGoalPositionWhenTimeLeft[currentGoalPositionPreset]:
+				currentGoalPositionPreset += 1
+				var tween = get_tree().create_tween()
+				tween.tween_property(goal, "position", goalPositionPresets[currentGoalPositionPreset], 1)
+			
 
 func on_game_over(_playerId: int):
 	print("show gameover")
