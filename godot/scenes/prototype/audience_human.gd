44b6cf2074453d24a52d643c636ab8f2b1beb356
snil4 <snil4@walla.co.il>
2025-01-25 17:58:13 +0200

    fixed audience placement

--- godot/scenes/prototype/audience_human.gd
+++ godot/scenes/prototype/audience_human.gd
@@ -12,10 +12,12 @@ var seat: int
 @export var curse_sounds2: AudioStream
 
 @export var walk_speed: float = 5
-@export var starting_side: Array = [-10, 890, 580]
+@export var starting_side: Array = [-20, 890, 580]
 # @export var starting_height: Array = [105, 186, 272, 366, 456]
-@export var seats: Array = [Vector2(50, 152), Vector2(167, 152), Vector2(64, 318), Vector2(129, 317), Vector2(239, 455), Vector2(311, 443), Vector2(377, 440),
-							Vector2(710, 368), Vector2(772, 368), Vector2(774, 272), Vector2(774, 193), Vector2(456, 437), Vector2(523, 444), Vector2(592, 455)]
+@export var seats: Array = [Vector2(50, 152), Vector2(167, 152), Vector2(64, 318), Vector2(129, 317), #Left side
+ 							Vector2(239, 455), Vector2(311, 443), Vector2(377, 440), Vector2(456, 437), Vector2(523, 444), Vector2(592, 455), #Middle
+							Vector2(710, 368), Vector2(772, 368), Vector2(774, 272), Vector2(774, 193),] #Right side
+
 # @export var player2_seats: Array = [770, 827]
 @export var body_sprites: Array = []
 @export var eyes_sprites: Array = []
@@ -30,6 +32,7 @@ var seat: int
 @export var curse_time := 8
 @export var chat_offset := Vector2()
 @export var curse_chance := 3
+@export var x_offset := 5.0
 
 @onready var body_node = $Body
 @onready var head_node = $Head
@@ -63,7 +66,7 @@ func start_tween():
 		animation_player.play("walk")
 
 	match seat:
-		0, 1, 2, 3, 7, 8, 9, 10:
+		4, 5, 6, 7, 8, 9:
 			tween.tween_property(self, "position:x", seat_target, walk_speed).set_trans(Tween.TRANS_QUART)
 		_:
 			tween.tween_property(self, "position:y", seat_target, walk_speed).set_trans(Tween.TRANS_QUART)
@@ -127,13 +130,13 @@ func change_player(_playerId: int):
 	# 		seat_target = player2_width[seat / 5]
 
 	match seat:
-		0, 1, 2, 3, 7, 8, 9, 10:
+		4, 5, 6, 7, 8, 9:
 			position.y = seats[seat].y
 			@warning_ignore("integer_division")
-			position.x = starting_side[(seat / 7)]
-			seat_target = seats[seat].x
+			position.x = starting_side[(seat / 7) - 1]
+			seat_target = seats[seat].x + randf_range(-x_offset, x_offset)
 		_:
-			position.x = seats[seat].x
+			position.x = seats[seat].x + randf_range(-x_offset, x_offset)
 			position.y = starting_side[2]
 			seat_target = seats[seat].y
 
